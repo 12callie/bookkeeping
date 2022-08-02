5a9eb460e265dea87b244e354e5fa3e0be52a50f
callie <2541474139@qq.com>
2022-08-02 22:48:14 +0800

    封装icon组件

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,23 +2,15 @@
   <div>
     <div class="nav">
       <router-link to="/money">
-        <svg>
-          <use xlink:href="#add" />
-        </svg>
+        <Icon name="add" />
         记账
       </router-link>
-      |
       <router-link to="/labels">
-        <svg>
-          <use xlink:href="#label" />
-        </svg>
+        <Icon name="label" />
         标签
       </router-link>
-      |
       <router-link to="/statistics">
-        <svg>
-          <use xlink:href="#statistics" />
-        </svg>
+        <Icon name="statistics" />
         统计
       </router-link>
     </div>
@@ -26,11 +18,6 @@
 </template>
 
 <script lang="ts">
-import x from "@/assets/icons/add.svg";
-import y from "@/assets/icons/label.svg";
-import z from "@/assets/icons/statistics.svg";
-console.log(x, y, z);
-
 export default {
   name: "Nav",
 };
@@ -39,9 +26,5 @@ export default {
 <style lang="scss" scoped>
 .nav {
   border: 1px solid blue;
-}
-svg {
-  width: 100px;
-  border: 1px solid red;
 }
 </style>
